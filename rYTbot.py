842878200fd16e1bead406ebb1060ef25a9525c7
RaiiDev <123031683+RaiiDev@users.noreply.github.com>
2023-01-21 02:35:21 -0300

    Update rYTbot.py

--- rYTbot.py
+++ rYTbot.py
@@ -61,7 +61,7 @@ for proxy in proxies:
     print("Verificando a proxy: " + proxy)
     try:
         # Usa a proxy para entrar no video.
-        response = requests.head(youtube_url, proxies={"http": "http://"+proxy, "https": "http://"+proxy}, timeout=35)
+        response = requests.head(youtube_url, proxies={"http": "http://"+proxy, "https": "http://"+proxy}, timeout=5)
         # Se a proxy retornar o valor 200, a proxy será bem sucedida
         if response.status_code == 200:
             valid_proxies.append(proxy)
@@ -80,10 +80,9 @@ else:
                 print(f"Usando a proxy: {valid_proxy}")
                 try:
                     # Use the proxy to access the video
-                    response = requests.get(youtube_url, proxies={"http": "http://"+valid_proxy, "https": "http://"+valid_proxy}, timeout=35)
+                    response = requests.get(youtube_url, proxies={"http": "http://"+valid_proxy, "https": "http://"+valid_proxy}, timeout=20)
                     if response.status_code == 200:
                         time.sleep(30)
-                        break
                 except requests.exceptions.RequestException:
                     print("Proxy " + valid_proxy + " inválida.")
    
@@ -92,9 +91,8 @@ else:
             print(f"Usando a proxy: {valid_proxy}")
             try:
                 # Use the proxy to access the video
-                response = requests.get(youtube_url, proxies={"http": "http://"+valid_proxy, "https": "http://"+valid_proxy}, timeout=35)
+                response = requests.get(youtube_url, proxies={"http": "http://"+valid_proxy, "https": "http://"+valid_proxy}, timeout=20)
                 if response.status_code == 200:
                     time.sleep(30)
-                    break
             except requests.exceptions.RequestException:
                 print("Proxy " + valid_proxy + " inválida.")
